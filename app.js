797da68e5e30dba98967593959fcadf1f77b9abf
Helder Balbino <helderbalbino@googlemail.com>
2025-02-06 22:39:02 +0000

    add: add API key constant for weather data retrieval in app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const weatherForm = document.querySelector('.weatherForm');
 const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
+const apiKey = 'YOUR_API_KEY_HERE';
