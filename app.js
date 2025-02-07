9f303a44bdf3b46ca0c5a1ca1656cd48a89f021c
Helder Balbino <helderbalbino@googlemail.com>
2025-02-07 08:59:20 +0000

    add: implement event listener for weather form submission and define weather data functions

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const weatherForm = document.querySelector('.weatherForm');
 const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
 const apiKey = 'YOUR_API_KEY_HERE';
+
+weatherForm.addEventListener('submit', (event) => {});
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
